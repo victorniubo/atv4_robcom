2b061132c506e4c996206723bd50ff2e7014c635
iStrawHat <igigouz@gmail.com>
2020-03-30 15:21:55 -0300

    half mouth

---
+++ projeto_creeper/scripts/atv4.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = ["Rachel P. B. Moraes", "Igor Montagner", "Fabio Miranda"]
+
+
+import rospy
+import numpy as np
+import tf
+import math
+import cv2
+import time
+from geometry_msgs.msg import Twist, Vector3, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage, LaserScan
+from cv_bridge import CvBridge, CvBridgeError
+import cormodule_mod
+
+
+bridge = CvBridge()
+
+cv_image = None
+media = []
+centro = []
+
+roxo = "#4c015b"
+verde = "#006507"
+azul = "#06355c"
+
+atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
+
+area = 0.0 # Variavel com a area do maior contorno
+
+# Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. 
+# Descarta imagens que chegam atrasadas demais
+check_delay = False 
+
+# A função a seguir é chamada sempre que chega um novo frame
+def roda_todo_frame(imagem):
+	print("frame")
+	global cv_image
+	global media
+	global centro
+	global maior_area
+
+	now = rospy.get_rostime()
+	imgtime = imagem.header.stamp
+	lag = now-imgtime # calcula o lag
+	delay = lag.nsecs
+	# print("delay ", "{:.3f}".format(delay/1.0E9))
+	if delay > atraso and check_delay==True:
+		print("Descartando por causa do delay do frame:", delay)
+		return 
+	try:
+		antes = time.clock()
+		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+		#cv_image = cv2.flip(cv_image, -1)
+		media, centro, maior_area =  cormodule_mod.identifica_cor(cv_image,verde)
+		depois = time.clock()
+		cv2.imshow("Camera", cv_image)
+	except CvBridgeError as e:
+		print('ex', e)
+
+def scaneou(dado):
+    global dist
+    # print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+    # print("Leituras:")   
+    dist = (np.array(dado.ranges).round(decimals=2))[0]
+    # print(x)
+    #print("Intensities")
+    #print(np.array(dado.intensities).round(decimals=2))
+	
+if __name__=="__main__":
+	rospy.init_node("cor")
+
+	# topico_imagem = "/kamera"
+	topico_imagem = "/camera/rgb/image_raw/compressed"
+	# Para renomear a *webcam*
+	#   Primeiro instale o suporte https://github.com/Insper/robot19/blob/master/guides/debugar_sem_robo_opencv_melodic.md
+	#
+	#	Depois faça:
+	#	
+	#	rosrun cv_camera cv_camera_node
+	#
+	# 	rosrun topic_tools relay  /cv_camera/image_raw/compressed /kamera
+	#
+	# 
+	# Para renomear a câmera simulada do Gazebo
+	# 
+	# 	rosrun topic_tools relay  /camera/rgb/image_raw/compressed /kamera
+	# 
+	# Para renomear a câmera da Raspberry
+	# 
+	# 	rosrun topic_tools relay /raspicam_node/image/compressed /kamera
+	# 
+
+	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+	print("Usando ", topico_imagem)
+
+	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+
+
+	try:
+
+		while not rospy.is_shutdown():
+			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+			if len(media) != 0 and len(centro) != 0:
+				# Calcula a diferença entre o centro do creeper e o centro da tela
+				dif = int(media[0]) - int(centro[0])
+				
+				if -30 < dif and dif < 30:
+					vel = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
+
+					if dist < 0.70:
+						vel = Twist(Vector3(0.05,0,0), Vector3(0,0,0))
+						print(420)
+					elif dist < 0.35:
+						vel = Twist(Vector3(0,0,0), Vector3(0,0,0))	
+						print("paro")
+
+				elif dif < -30:
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,0.2))
+
+				elif dif > 30:
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
+				
+				
+
+				print("Distancia: {0}".format(dist))
+				# print("Area:{0}".format(maior_area))
+				# print("Média cor: {0}, {1}".format(media[0], media[1]))
+				# print("Centro tela: {0}, {1}".format(centro[0], centro[1]))
+			velocidade_saida.publish(vel)
+			rospy.sleep(0.1)
+
+	except rospy.ROSInterruptException:
+	    print("Ocorreu uma exceção com o rospy")
---
+++ projeto_creeper/scripts/cormodule_mod.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import rospy
+import numpy as np
+import tf
+import math
+import cv2
+import time
+from geometry_msgs.msg import Twist, Vector3, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+import smach
+import smach_ros
+
+
+def convert_to_tuple(html_color):
+    colors = html_color.split("#")[1]
+    r = int(colors[0:2],16)
+    g = int(colors[2:4],16)
+    b = int(colors[4:],16)
+    return (r,g,b)
+
+def to_1px(tpl):
+    img = np.zeros((1,1,3), dtype=np.uint8)
+    img[0,0,0] = tpl[0]
+    img[0,0,1] = tpl[1]
+    img[0,0,2] = tpl[2]
+    return img
+
+def to_hsv(html_color):
+    tupla = convert_to_tuple(html_color)
+    hsv = cv2.cvtColor(to_1px(tupla), cv2.COLOR_RGB2HSV)
+    return hsv[0][0]
+
+def ranges(value):
+    hsv = to_hsv(value)
+    hsv2 = np.copy(hsv)
+    hsv[0] = max(0, hsv[0]-10)
+    hsv2[0] = min(180, hsv[0]+ 10)
+    hsv[1:] = 50
+    hsv2[1:] = 255
+    return hsv, hsv2 
+
+
+def identifica_cor(frame,cor):
+    '''
+    Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
+    '''
+
+    # No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
+    # vermelho puro (H=0) estão entre H=-8 e H=8. 
+    # Precisamos dividir o inRange em duas partes para fazer a detecção 
+    # do vermelho:
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    hsv0, hsv1 = ranges(cor)
+
+
+    segmentado_cor = cv2.inRange(frame_hsv, hsv0, hsv1)
+
+    # Note que a notacão do numpy encara as imagens como matriz, portanto o enderecamento é
+    # linha, coluna ou (y,x)
+    # Por isso na hora de montar a tupla com o centro precisamos inverter, porque 
+    centro = (frame.shape[1]//2, frame.shape[0]//2)
+
+
+    def cross(img_rgb, point, color, width,length):
+        cv2.line(img_rgb, (point[0] - length/2, point[1]),  (point[0] + length/2, point[1]), color ,width, length)
+        cv2.line(img_rgb, (point[0], point[1] - length/2), (point[0], point[1] + length/2),color ,width, length) 
+
+
+
+    # A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
+    # que um quadrado 7x7. É muito útil para juntar vários 
+    # pequenos contornos muito próximos em um só.
+    segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
+
+    # Encontramos os contornos na máscara e selecionamos o de maior área
+    #contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)	
+    contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+
+    maior_contorno = None
+    maior_contorno_area = 0
+
+    for cnt in contornos:
+        area = cv2.contourArea(cnt)
+        if area > maior_contorno_area:
+            maior_contorno = cnt
+            maior_contorno_area = area
+
+    # Encontramos o centro do contorno fazendo a média de todos seus pontos.
+    if not maior_contorno is None :
+        cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
+        maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
+        media = maior_contorno.mean(axis=0)
+        media = media.astype(np.int32)
+        cv2.circle(frame, (media[0], media[1]), 5, [0, 255, 0])
+        cross(frame, centro, [255,0,0], 1, 17)
+    else:
+        media = (0, 0)
+
+    # Representa a area e o centro do maior contorno no frame
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    cv2.putText(frame,"{:d} {:d}".format(*media),(20,100), 1, 4,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame,"{:0.1f}".format(maior_contorno_area),(20,50), 1, 4,(255,255,255),2,cv2.LINE_AA)
+
+   # cv2.imshow('video', frame)
+    #cv2.imshow('seg', segmentado_cor)
+    cv2.waitKey(1)
+
+    return media, centro, maior_contorno_area
